7ea028717a199e07027fe94256627b8bec09d411
Antonio Zanon <antonio.zanon99@gmail.com>
2025-07-19 13:57:37 -0300

    Teste 2 - Correção do arquivo proxy

--- proxy.php
+++ proxy.php
@@ -3,11 +3,19 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
 $url = $_GET['url'] ?? '';
+
 if (!$url || !filter_var($url, FILTER_VALIDATE_URL)) {
     http_response_code(400);
     echo json_encode(['erro' => 'URL inválida']);
     exit;
 }
 
-$response = file_get_contents($url);
+$response = @file_get_contents($url);
+
+if ($response === false) {
+    http_response_code(500);
+    echo json_encode(['erro' => 'Erro ao acessar a URL: ' . $url]);
+    exit;
+}
+
 echo $response;
